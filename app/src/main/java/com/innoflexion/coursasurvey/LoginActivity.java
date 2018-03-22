8ac3f4b14d6bc6037c4d2c153f3d87eb311e1dc3
Satyam <satyam.sutapalli@innoflexion.com>
2018-03-22 12:02:52 +0530

    handling valdiation in login

--- app/src/main/java/com/innoflexion/coursasurvey/LoginActivity.java
+++ app/src/main/java/com/innoflexion/coursasurvey/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by raghu on 3/19/2018.
@@ -12,7 +13,9 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btn_login;
+    private Button btn_login;
+    private EditText userName;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +23,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn_login = (Button) findViewById(R.id.btn_login);
+        userName = (EditText) findViewById(R.id.email);
+        password =(EditText) findViewById(R.id.password);
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(userName.getText().toString().trim().length() == 0) {
+                    userName.setError("Username is not entered");
+                    userName.requestFocus();
+                }
+                if(password.getText().toString().trim().length() == 0){
+                    password.setError("Password is not entered");
+                    password.requestFocus();
+                }
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
